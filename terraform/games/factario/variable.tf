ce8aef52988aa1a2e5982b6660f669c827e43336
Scott Lemmon <skot.lemmon@gmail.com>
2020-10-03 22:20:22 -0700

    begin adding factario server plugin

---
+++ terraform/games/factario/variable.tf
@@ -0,0 +1,59 @@
+variable "aws_region" {
+  description = "the aws region the server is running in"
+}
+
+variable "name_prefix" {
+  description = "A prefix to add to resource names to ensure uniqueness and easy searchabilty"
+  default     = ""
+}
+
+variable "username" {
+  description = "a username to use for sshing into running machines"
+}
+
+variable "pub_ssh_key" {
+  description = "a public ssh key associated with the user for sshing into running machines"
+}
+
+variable "hosted_zone_id" {
+  description = "the hosted zone id your domain lives in."
+}
+
+variable "tools_docker_image_id" {
+  description = "the docker image with the tools for checking and changing status of the server"
+  default     = "tlemmon/spot-tools:latest"
+}
+
+variable "docker_image" {
+  description = "the docker image to use"
+  default     = "factoriotools/factorio:0.18.10"
+}
+
+variable "bucket_name" {
+  description = "the name for an s3 bucket which will be created to store game data, both backups and periodic saves"
+}
+
+variable "subdomain" {
+  description = "the subdomain name to run the instance at"
+  default     = "factario"
+}
+
+variable "no_user_grace_period" {
+  description = "the amount of time (in seconds) no user can be present on the server before it terminates itself"
+  default     = "1800"
+}
+
+variable "backup_command" {
+  description = "the command to use to cause the server to backup"
+  default     = "rcon-cli ftb backup start"
+}
+
+variable "backups_path" {
+  description = "the path where the server will backup relative to the minecraft data path"
+  default     = "FeedTheBeast/backups"
+}
+
+variable "backup_index_path" {
+  description = "the path where the server will save the backup index relative to the minecraft data path"
+  default     = "FeedTheBeast/backups/backups.json"
+}
