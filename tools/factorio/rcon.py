bf7106c69e6acce7bc88b67ed9a1b90c7e61c887
Scott Lemmon <skot.lemmon@gmail.com>
2020-10-06 00:38:12 -0700

    Strip whitespace from rcon password

--- tools/factorio/rcon.py
+++ tools/factorio/rcon.py
@@ -7,7 +7,7 @@ GAME_DATA = '/data'
 def get_rcon_client():
     if not get_rcon_client._client:
         with open(os.path.join(GAME_DATA, 'config/rconpw'), 'r') as fin:
-                rconpw = fin.read()
+                rconpw = fin.read().strip()
         get_rcon_client._client = factorio_rcon.RCONClient("factorio", 27015, rconpw)
     return get_rcon_client._client
 get_rcon_client._client = None
