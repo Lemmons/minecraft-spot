057afafb705556bbe384fb1be242c4b67b8f7321
Scott Lemmon <skot.lemmon@gmail.com>
2020-10-07 14:09:03 -0700

    Fix factorio player count

--- tools/factorio/players.py
+++ tools/factorio/players.py
@@ -3,14 +3,14 @@ import re
 import spot_tools.instance
 import spot_tools.factorio.rcon
 
-PLAYERS_RE = re.compile(r'Players \((?P<players>\d*)\):.*')
+PLAYERS_RE = re.compile(r'Online players \((?P<players>\d*)\):.*')
 def get_players():
     instance = spot_tools.instance.get_instance()
     if instance.status == "exited":
         return
 
     rcon = spot_tools.factorio.rcon.get_rcon_client()
-    result = rcon.send_command('/players count')
+    result = rcon.send_command('/players online')
 
     players = int(PLAYERS_RE.match(result).group('players'))
 
